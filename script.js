b392ce3e88ebaada75936d6a9a93678197cf051c
ncallan2019 <44303814+ncallan2019@users.noreply.github.com>
2019-02-01 16:54:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ window.onload = a()
 function a() {
   x = window.location.pathname.substring(1).replace(/-/g, "/");
   document.title = "ISP Journal " + x;
-  document.getElementById("title") = "ISP Journal " + x;
+  document.getElementById("title").innerHTML = "ISP Journal " + x;
 }
