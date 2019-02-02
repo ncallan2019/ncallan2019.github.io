f5ddef8357ca2bb09afde91d8397afc197709d47
ncallan2019 <44303814+ncallan2019@users.noreply.github.com>
2019-02-02 17:11:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+
+
 window.onload = a;
 
 function a() {
   x = window.location.pathname.substring(1).replace(/-/g, "/").replace(/\.html?/g, "");
   document.title = "ISP Journal " + x;
   document.getElementById("date").innerHTML = x;
+  
+  top = document.getElementsByTagName("select").item(0)
+  bottom = document.getElementsByTagName("select").item(1)
+  
+  for (var key in p) {
+    if (p.hasOwnProperty(key)) {
+        .innerHTML+="<option>dfdf</option>"
+    }
+  }
 }
