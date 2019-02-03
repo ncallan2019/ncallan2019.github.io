53ae8d6cf04ad6a042a737ab1740a85e810df99f
ncallan2019 <44303814+ncallan2019@users.noreply.github.com>
2019-02-02 21:36:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,14 @@ function a() {
   document.title = "ISP Journal " + x;
   document.getElementById("date").innerHTML = x;
   
-  var sel = document.getElementById('topNav');
+  var sel1 = document.getElementById('topNav');
+  var sel2 = document.getElementById('bottomNav');
   for (var i = 0; i < entries.length; i++) {
     var opt = document.createElement('option');
     opt.innerHTML = entries[i];
     opt.value = entries[i];
-    sel.appendChild(opt);
+    sel1.appendChild(opt);
+    sel2.appendChild(opt)
   };
   /*var topNav = document.getElementsByTagName("select").item(0)
   var bottomNav = document.getElementsByTagName("select").item(1)
